880c60fa344900404fe9d938478db4f3b3451e69
RamonLSilva <ramonlimadasilva63@gmail.com>
2024-07-29 11:56:24 -0300

    Pizzaria2A_ITB

--- src/pages/Listagem.jsx
+++ src/pages/Listagem.jsx
@@ -20,8 +20,8 @@ const Listagem = () => {
 
     return (
         <>
-            <h2 style={{textAlign: 'center'}}>
-                Pizzas ITB
+            <h2 style={{textAlign: 'center', color: 'white'}}>
+                Pizzas2A ITB
             </h2>
             <ul>
                  <ItensLista />
